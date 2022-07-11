05a865e6529bc3eade9be95ecd4cb350d825c8f6
hadleydalt <hadley_dalton@brown.edu>
2022-07-10 18:57:03 -0700

    refactor: change text

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Hi my name is <code>Hadley</code> and I like to eat rhinos
         </p>
         <a
           className="App-link"
