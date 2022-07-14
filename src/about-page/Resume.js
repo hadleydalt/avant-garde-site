630c5688e5c33343da46b08113e67340443782a4
hadleydalt <hadley_dalton@brown.edu>
2022-07-13 17:53:17 -0700

    refactor: resume styling config

--- src/about-page/Resume.js
+++ src/about-page/Resume.js
@@ -3,8 +3,8 @@ import '../App.css'
 export const Resume = () => {
   return (
     <>
-    <div style={{paddingLeft: '5vw', paddingRight: '5vw', marginTop: '5vh', background: '#00C0C9', borderRadius: '10px', padding: '1vw', color: 'white', textTransform: 'uppercase'}}>Click to download</div>
-    <img src={require('../graphics/Resume.png')} alt="" style={{width: '70%'}} />
+    <div className="resume-download">Click to download</div>
+        <img src={require('../graphics/Resume.png')} alt="" style={{width: '70%'}} />
     </>
   )
 }
